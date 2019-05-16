757fbff2d4777208fd3b73bc72a7ae2e78fcb72e
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-16 22:36:49 +0900

    低い数値の場合は文字を赤くする

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -18,6 +19,11 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.TextItemViewHold
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val item = data[position]
+        if (item.sleepQuality <= 1) {
+            holder.textView.setTextColor(Color.RED)
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
+        }
         holder.textView.text = item.sleepQuality.toString()
     }
 
